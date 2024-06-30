34faef1f3fbd396e2a767db97936b2f6699f568f
Paramdeep Singh <paramdeep@meta.com>
2024-06-30 13:16:48 -0700

    marked variables5 done; solved variables6; updated README to include my notes on Shadowing in Rust

--- exercises/01_variables/README.md
+++ exercises/01_variables/README.md
@@ -7,3 +7,14 @@ You can make them mutable by adding `mut` in front of the variable name.
 ## Further information
 
 - [Variables and Mutability](https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html)
+
+## My Notes!
+
+##### Unveiling the magic of "shadowing"
+The OS knows nothing about "shadowing" - it is a purely a construct of the Rust compiler. So a 
+Rust programs with shadowing, and a corresponding Rust program with different variable names (for
+each subsequent shadowing) will behave exactly the same. Only difference is that Rust explicitly 
+makes the "shadowED" variable inaccessible. Otherwise, in terms of lifecycle and stuff, just treat
+"shadowED" and "shadowING" variables as two different variables (with different names), with the
+former now inaccessible (enforced by the Rust compiler). 
+References: https://www.reddit.com/r/rust/comments/17d6o4v/im_confused_about_shadowing/
--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -5,7 +5,23 @@
 
 // I AM NOT DONE
 
-const NUMBER = 3;
+/*
+    Constants in Rust must have their type specified because they are evaluated at compile-time, 
+    while variables defined with let can have their type inferred because they are evaluated at 
+    runtime. Thus constants are compile-time creatures, and the compiler needs all the support 
+    possible to understand its value, type, and then effectively paste it wherever it is referenced.
+ */
+const NUMBER: i32 = 3;
 fn main() {
+    /*
+        Wanted to experiment if Rust "shadowing" works for constants as well, and to my surprise, it 
+        does! After some reading, it does make sense: from a compiler's perspective it just needs
+        to paste the constant value wherever it is reference in the code, so there shouldn't be any
+        problem if the programmer at some point wants to "update" what is meant by that constant
+        name. The compiler will just paste that "new" value denoted by the constant name going 
+        forward. Also, reference the notes under "Unveiling the magic of "shadowing" under this 
+        folder's README
+     */
+    // const NUMBER: i32 = 4;
     println!("Number {}", NUMBER);
 }
