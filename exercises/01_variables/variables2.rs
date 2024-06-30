bc4f6c9c242b113a40530b9cf8814706dd733f50
Paramdeep Singh <paramdeep@meta.com>
2024-06-30 12:14:58 -0700

    done with variables2; solved variables3

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     /*
     Q: I've begun learning the Rust programming language. Why does Rust need the programmer to 
