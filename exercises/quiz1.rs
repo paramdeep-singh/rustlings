225c792a6f03924f6c62a8caa71826cf338cf6c6
Paramdeep Singh <paramdeep@meta.com>
2024-06-30 15:57:26 -0700

    marked if3 done; solved quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,9 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    if amount <= 40 { 2 * amount } else { amount }
+}
 
 // Don't modify this function!
 #[test]
