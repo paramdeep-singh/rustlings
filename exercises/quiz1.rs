afa909840d3db8fa5d18d7ef632cbd419c0d7b79
Paramdeep Singh <paramdeep@meta.com>
2024-06-30 18:44:39 -0700

    marked quiz1 done; solved primitive_types1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 fn calculate_price_of_apples(amount: i32) -> i32 {
     if amount <= 40 { 2 * amount } else { amount }
