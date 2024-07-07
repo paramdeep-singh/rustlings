d8019859f0fcf7676ad80a6b38cb4014bac2aa4e
Paramdeep Singh <paramdeep@meta.com>
2024-07-07 15:01:02 -0700

    marked vecs2 done; solved move_semantics1

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -7,17 +7,21 @@
 
 #[test]
 fn main() {
+    // stack variable 'vec0' now points to a vector allocated in the heap
     let vec0 = vec![22, 44, 66];
 
+    // vec0 lost ownership here
     let vec1 = fill_vec(vec0);
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec; // shadowing to mutable
 
     vec.push(88);
 
-    vec
+    // here the ownership of the heap allocated vector is passed to the calling function, and so
+    // the vector is not dropped in the heap since the owner is not here
+    vec 
 }
