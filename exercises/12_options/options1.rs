a46eb0044a22eb11eb77f2673cd9f71ae486f9c8
Paramdeep Singh <paramdeep@meta.com>
2024-07-13 17:53:25 -0700

    marked hashmaps3 done; solved options1

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -13,7 +13,13 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0. The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    if time_of_day < 22 {
+        return Some(5);
+    } else if time_of_day >= 22 && time_of_day < 24 {
+        return Some(0)
+    } else {
+        return None;
+    }
 }
 
 #[cfg(test)]
@@ -35,6 +41,6 @@ mod tests {
         // TODO: Fix this test. How do you get at the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams.unwrap(), 5);
     }
 }
