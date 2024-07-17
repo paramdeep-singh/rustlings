094d8e43b9b65af3fa20855d714d9c5514f5d697
Paramdeep Singh <paramdeep@meta.com>
2024-07-16 21:45:31 -0700

    explanation for the case of two lifetimes on a struct

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -7,6 +7,21 @@
 
 // I AM NOT DONE
 
+/*
+    While implementing this solution, I got curious on when would TWO lifetimes be specified on a 
+    struct (say if it has two slice references on it). After much thought, and playing with the 
+    open source code here (https://github.com/tfpk/lifetimekata/blob/main/exercises/05_lifetimes_on_types/solutions/src/lib.rs)
+    I concluded, just like the repo author here (https://github.com/tfpk/lifetimekata/blob/main/exercises/05_lifetimes_on_types/README.md#two-lifetimes)
+    that you would need to do so, say if the first string (source of first field's reference(s)) 
+    outlives the second string (source of second field's reference(s)) in the calling code. Why? 
+    1. Okay for starters, the struct itself has to be dropped/inaccessible after one of its 
+    reference strings is dropped, because of lifetimes relationships. 
+    2. If the both fields on the struct have the same lifetime, then you can't even move the other
+    string's references out of the struct before it destroys because same lifetimes mean it can't
+    be borrowed.
+    3. One can at least move the first string's references (aka first field of the struct) out of 
+    the struct - before it is dropped - if the lifetimes are defined different for the fields.
+*/
 struct Book<'a> {
     author: &'a str,
     title: &'a str,
