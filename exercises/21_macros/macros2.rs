bd35fbb588f15087ae87b118c5a5854481f88fa1
Paramdeep Singh <paramdeep@meta.com>
2024-07-24 19:17:42 -0700

    marked macros1 done; solved macros2

--- exercises/21_macros/macros2.rs
+++ exercises/21_macros/macros2.rs
@@ -5,12 +5,13 @@
 
 // I AM NOT DONE
 
-fn main() {
-    my_macro!();
-}
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
 }
+
+fn main() {
+    my_macro!();
+}
+
